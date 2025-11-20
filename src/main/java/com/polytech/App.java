bdfbdefaa6a7871bde67176dae0996498ba02509
manel <manelarama0@gmail.com>
2025-11-20 15:38:06 +0100

    Implémentation du premier exercice

--- src/main/java/com/polytech/App.java
+++ src/main/java/com/polytech/App.java
@@ -1,5 +1,9 @@
 package com.polytech;
 
+import com.polytech.tp.CoursBuilder;
+import com.polytech.tp.CoursEnLigne;
+import com.polytech.tp.ICours;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       ICours coursBase = new CoursBuilder()
+        .setMatiere("Génie Logiciel")
+        .setEnseignant("Mme Manel")
+        .setSalle("B203")
+        .build();
+
+ICours coursEnLigne = new CoursEnLigne(coursBase);
+
+System.out.println(coursEnLigne.getDescription());
     }
 }
--- src/main/java/com/polytech/tp/CoursEnLigne.java
+++ src/main/java/com/polytech/tp/CoursEnLigne.java
@@ -1,21 +1,24 @@
 package com.polytech.tp;
 
 public class CoursEnLigne extends CoursDecorator {
-    CoursEnLigne(ICours cours) {
+     public CoursEnLigne(ICours cours) {
         //TODO: Implémenter le reste ...
+         super(cours);
     }
 
     @Override
     public String getDescription() {
         // TODO Auto-generated method stub
+        return coursDecorated.getDescription() + " (En ligne)";
         
-        return null;
+       
     }
 
     @Override
     public double getDuree() {
         // TODO Auto-generated method stub
+          return coursDecorated.getDuree();
         
-        return 0;
+  
     };
 }
